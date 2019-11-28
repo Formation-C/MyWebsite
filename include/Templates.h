ad5f4247ab671b11ff8d94486c9c12f5491c330a
Romain Tello <romain.tello@gmail.com>
2019-11-28 11:22:53 +0100

    added comments to Templates.h

--- include/Templates.h
+++ include/Templates.h
@@ -7,19 +7,26 @@
 
 namespace Templates
 {
+    // Définit un modèle de fonction qui va pouvoir prendre n'importe quel type T
     template<typename T>
+    // Méthode permettant de filtrer les objets en fonction d'un critère précis
     std::vector<T*> Filterobjects(std::function<bool(T*)> validator) {
+        // Initialise le vecteur à renvoyer
         std::vector<T*> result;
 
-        //std::vector<T*>::iterator iter, iter_end;
+        // Initialise les itérateurs pour parcourir le vecteur
         auto iter = T::objects.begin();
         auto iter_end = T::objects.end();
         for (; iter != iter_end; ++iter) {
+            // Récupère la référence de l'éléménet actuellement parcouru
             T* object = *iter;
+            // Exécute la fonction de validation spécifiée par l'appelant
             if (validator(object)) {
+                // Si la fonction valide l'objet, l'ajoute dans le vecteur à renvoyer
                 result.push_back(object);
             }
         }
+        // Renvoie le résultat
         return result;
     }
 };
