1c22d1ab917f0bd9249b2c3fc13413ef913ca9e8
Romain Tello <romain.tello@gmail.com>
2019-11-28 09:23:14 +0100

    removed comments

--- src/Templates.cpp
+++ src/Templates.cpp
@@ -1,15 +1,1 @@
 #include "Templates.h"
-
-//template<typename T>
-//std::vector<T> Template::Filterobjects(function<T> validator) {
-//    std::vector<T*> result;
-//
-//    std::vector<T*>::iterator iter, iter_end;
-//    for (iter = T::objects.begin(), iter_end = T::objects.end(); iter != iter_end; ++iter) {
-//        T* object = *iter;
-//        if (validator(object)) {
-//            result.push_back(object);
-//        }
-//    }
-//    return result;
-//}
